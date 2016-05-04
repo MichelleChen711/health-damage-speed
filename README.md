0d8246b5d13cf08da7fbec80a7f77f596487fc55
Michelle Chen <mc4514@nyu.edu>
2016-05-04 00:50:49 -0400

    missiles work, added collision function

---
+++ README.md
@@ -0,0 +1,7 @@
+Requirements:
+pysdl2
+turtle
+pysound
+(additional features to be added)
+
+pysdl2 turned out to be quite difficult to implement. There are not lot of information guides on the basics of using SDL2 in Python for games available. Most of them is in C and C++ which made it difficult to start with. 
--- game.py
+++ game.py
@@ -10,6 +10,8 @@ WHITE = sdl2.ext.Color(255, 255, 255)
 BLACK = sdl2.ext.Color(0, 0, 0)
 RED = sdl2.ext.Color(255, 0, 0)
 GREEN = sdl2.ext.Color(0, 255, 0)
+BLUE = sdl2.ext.Color(0, 0, 255)
+
 RESOURCES = sdl2.ext.Resources(__file__, "resources")
 
 
@@ -44,24 +46,25 @@ class Player(sdl2.ext.Entity):
 
 
 
-class Missile1(sdl2.ext.Entity):
-    def __init__(self, world, sprite, damage, posx=0, posy=0):
+class Missile(sdl2.ext.Entity):
+    def __init__(self, world, sprite, damage, vx, posx=0, posy=0):
         self.sprite = sprite
         self.sprite.damage = damage
         self.sprite.position = posx, posy
         self.sprite.x = posx
         self.sprite.y = posy
+        self.sprite.vx = vx
 
 
-def renderHPBar2(x, y, w, h, percent, fgColor, bgColor, surface):
-    if percent > 1:
-        percent = 1
-    elif percent < 0:
-        percent = 0
+def renderHPBar2(x, y, w, h, health, fgColor, bgColor, surface):
+    if health > 18:
+        health = 18
+    elif health < 0:
+        health = 0
 
     rect1 = (x,y,w,h)
 
-    pw = w * percent
+    pw = w * (health/18)
     px = x + (w - pw)
     rect2 = (px,y,pw,h)
 
@@ -69,15 +72,15 @@ def renderHPBar2(x, y, w, h, percent, fgColor, bgColor, surface):
     sdl2.ext.fill(surface, fgColor, rect2)
 
 
-def renderHPBar1(x, y, w, h, percent, fgColor, bgColor, surface):
-    if percent > 1:
-        percent = 1
-    elif percent < 0:
-        percent = 0
+def renderHPBar1(x, y, w, h, health, fgColor, bgColor, surface):
+    if health > 18:
+        health = 1
+    elif health < 0:
+        health = 0
 
     rect1 = (x,y,w,h)
 
-    pw = w * percent
+    pw = w * (health/18)
     pw = pw
     #px = x + (w - pw)
     rect2 = (x,y,pw,h)
@@ -85,8 +88,22 @@ def renderHPBar1(x, y, w, h, percent, fgColor, bgColor, surface):
     sdl2.ext.fill(surface, bgColor, rect1)
     sdl2.ext.fill(surface, fgColor, rect2)
 
+def moveMissile(missile):
+    missile.sprite.x += missile.sprite.vx
+
+
 def check_cooldown(now_time, last_fire_time):
     return float(now_time) - float(last_fire_time)
+
+
+def collisonDetect(mx,my,mwidth,cx,cy,cwidth):
+    # right side of missile is more than left of character
+    # top of missile is less than bottom of character and more than top of character
+    if (mx + mwidth > cx) and  (my < cy + cwidth) and (my > cy):
+        return True
+    else:
+        return False
+
 
 def run():
     sdl2.ext.init()
@@ -106,88 +123,187 @@ def run():
 
     windowsurface = window.get_surface()
 
-    player1 = Player(world, sprite1, 10, 5, 5, 0, 250)
+    player1 = Player(world, sprite1, 5, 5, 10, 0, 250)
     player2 = Player(world, sprite2, 8, 10, 2, 790, 250)
 
-    missileSprite1 = factory.from_color(RED,size=(player1.sprite.playerdata.damage * 2,player1.sprite.playerdata.damage * 2))
+    missileSprite1 = factory.from_color(RED, size=(player1.sprite.playerdata.damage * 5,
+                                                   player1.sprite.playerdata.damage * 5))
+    missileSprite2 = factory.from_color(BLUE, size=(player2.sprite.playerdata.damage * 5,
+                                                    player2.sprite.playerdata.damage * 5))
+    missileSprite1_2 = factory.from_color(RED, size=(player1.sprite.playerdata.damage * 5,
+                                                   player1.sprite.playerdata.damage * 5))
+    missileSprite2_2 = factory.from_color(BLUE, size=(player2.sprite.playerdata.damage * 5,
+                                                    player2.sprite.playerdata.damage * 5))
+    missileSprite1_3 = factory.from_color(RED, size=(player1.sprite.playerdata.damage * 5,
+                                                   player1.sprite.playerdata.damage * 5))
+    missileSprite2_3 = factory.from_color(BLUE, size=(player2.sprite.playerdata.damage * 5,
+                                                    player2.sprite.playerdata.damage * 5))
 
 
     player1missiles = []
     player2missiles = []
+    player1missiles2 = []
+    player2missiles2 = []
+    player1missiles3 = []
+    player2missiles3 = []
+
 
     sdl2.ext.fill(windowsurface, WHITE)
 
-    last_fire_time = 0
+    last_fire_time1 = 0
+    last_fire_time1_1 = 0
+    last_fire_time2 = 0
+    last_fire_time2_1 = 0
+
+
+    sdl2.ext.fill(windowsurface, WHITE)
+    #renderHPBar1(30, 10, 400, 40, 1, GREEN, BLACK, windowsurface)
+    #renderHPBar2(480, 10, 400, 40, 1, GREEN, BLACK, windowsurface)
+
 
     running = True
     while running:
 
         #full health 18 = 1 percentage
         sdl2.ext.fill(windowsurface, WHITE)
-        renderHPBar1(30, 10, 400, 40, 1, GREEN, BLACK, windowsurface)
-        renderHPBar2(480, 10, 400, 40, 1, GREEN, BLACK, windowsurface)
+        renderHPBar1(30, 10, 400, 40, player1.sprite.playerdata.health, GREEN, BLACK, windowsurface)
+
+        renderHPBar2(480, 10, 400, 40, player2.sprite.playerdata.health, GREEN, BLACK, windowsurface)
 
         now_time = time.time()
+
+        if len(player1missiles) > 0:
+            moveMissile(player1missiles[0])
+            if player1missiles[0].sprite.x >= 900 + missileSprite1.size[0]:
+                player1missiles.pop(0)
+
+        if len(player1missiles2) > 0:
+            moveMissile(player1missiles2[0])
+            if player1missiles2[0].sprite.x >= 900 + missileSprite1_2.size[0]:
+                player1missiles2.pop(0)
+
+        if len(player1missiles3) > 0:
+            moveMissile(player1missiles3[0])
+            if player1missiles3[0].sprite.x >= 900 + missileSprite1_3.size[0]:
+                player1missiles3.pop(0)
+
+        if len(player2missiles) > 0:
+            moveMissile(player2missiles[0])
+            if player2missiles[0].sprite.x <= 0 - missileSprite2.size[0]:
+                player2missiles.pop(0)
+
+        if len(player2missiles2) > 0:
+            moveMissile(player2missiles2[0])
+            if player2missiles2[0].sprite.x <= 0 - missileSprite2_2.size[0]:
+                player2missiles2.pop(0)
+
+        if len(player2missiles3) > 0:
+            moveMissile(player2missiles3[0])
+            if player2missiles3[0].sprite.x <= 0 - missileSprite2_3.size[0]:
+                player2missiles3.pop(0)
+
+
+        #missile1 = Missile1(world, missileSprite1, player1.sprite.playerdata.damage,0,0)
         for event in sdl2.ext.get_events():
             if event.type == sdl2.SDL_QUIT:
                 running = False
                 break
-            if event.type == sdl2.SDL_KEYDOWN:
-                if event.key.keysym.sym == sdl2.SDLK_SPACE:
-                    # shoot a missile based on character damage
-                    player1.sprite.missiles += 1
-                    if player1.sprite.missiles <= 3:
-                        missile1 = Missile1(world, missileSprite1, player1.sprite.playerdata.damage,
-                                            player1.sprite.x + 50, player1.sprite.y + 80)
-                        player1missiles.append(missile1)
-                    else:
-                        player1.sprite.missiles = 0
+            #if event.type == sdl2.SDL_KEYDOWN:
+                #if event.key.keysym.sym == sdl2.SDLK_SPACE:
 
         keystatus = sdl2.SDL_GetKeyboardState(None)
 
-        # shoot a missile based on character damage
         if keystatus[sdl2.SDL_SCANCODE_SPACE]:
-            print(player1missiles)
-            print(now_time, float(now_time- last_fire_time))
-            if check_cooldown(now_time,last_fire_time) > 2:
-                last_fire_time = time.time()
-                #player1.sprite.missiles += 1
-                missile1 = Missile1(world, missileSprite1, player1.sprite.playerdata.damage, player1.sprite.x + 50, player1.sprite.y + 80)
+            if len(player1missiles) == 0:
+                missile1 = Missile(world, missileSprite1, player1.sprite.playerdata.damage, 8,
+                                   player1.sprite.x + 50, player1.sprite.y + missileSprite1.size[0])
                 player1missiles.append(missile1)
-                cd_clock_start = time.time()
+                last_fire_time1 = time.time()
+                last_fire_time1_1 = time.time()
+            if check_cooldown(now_time, last_fire_time1) > .2:
+                if len(player1missiles2) == 0:
+                    missile2 = Missile(world, missileSprite1_2, player1.sprite.playerdata.damage, 8,
+                                    player1.sprite.x + 50, player1.sprite.y + missileSprite1.size[0])
+                    player1missiles2.append(missile2)
+                    last_fire_time1 = time.time()
+                    last_fire_time1_1 = time.time()
+            if check_cooldown(now_time, last_fire_time1_1) > .2:
+                if len(player1missiles3) == 0:
+                    missile3 = Missile(world, missileSprite1_3, player1.sprite.playerdata.damage, 8,
+                                    player1.sprite.x + 50, player1.sprite.y + missileSprite1.size[0])
+                    player1missiles3.append(missile3)
+                    last_fire_time1_1 = time.time()
+                    last_fire_time1 = time.time()
+
+        # shoot a missile based on character damage
+        if keystatus[sdl2.SDL_SCANCODE_RETURN]:
+            if len(player2missiles) == 0:
+                missile1 = Missile(world, missileSprite2, player2.sprite.playerdata.damage, -8,
+                                   player2.sprite.x + 50, player2.sprite.y + missileSprite2.size[0])
+                player2missiles.append(missile1)
+                last_fire_time2 = time.time()
+                last_fire_time2_1 = time.time()
+            if check_cooldown(now_time, last_fire_time2) > .2:
+                if len(player2missiles2) == 0:
+                    missile2 = Missile(world, missileSprite2_2, player2.sprite.playerdata.damage, -8,
+                                    player2.sprite.x + 50, player2.sprite.y + missileSprite2.size[0])
+                    player2missiles2.append(missile2)
+                    last_fire_time2 = time.time()
+                    last_fire_time2_1 = time.time()
+            if check_cooldown(now_time, last_fire_time2_1) > .2:
+                if len(player2missiles3) == 0:
+                    missile3 = Missile(world, missileSprite2_3, player2.sprite.playerdata.damage, -8,
+                                    player2.sprite.x + 50, player2.sprite.y + missileSprite2.size[0])
+                    player2missiles3.append(missile3)
+                    last_fire_time2_1 = time.time()
+                    last_fire_time2 = time.time()
+            """
+           if len(player2missiles) == 0:
+               missile2 = Missile(world, missileSprite2, player2.sprite.playerdata.damage, -10,
+                                   player2.sprite.x + 50, player2.sprite.y + missileSprite2.size[0])
+               player2missiles.append(missile2)"""
+
+            #print(now_time, float(now_time- last_fire_time))
+            #if check_cooldown(now_time,last_fire_time) > 2:
+                #last_fire_time = time.time()
+                #player1.sprite.missiles += 1
+                #missile1 = Missile1(world, missileSprite1, player1.sprite.playerdata.damage, player1.sprite.x + 50, player1.sprite.y + 80)
+                #player1missiles.append(missile1)
+                #cd_clock_start = time.time()"""
 
         if keystatus[sdl2.SDL_SCANCODE_W]:
             player1.vy = player1.sprite.playerdata.speed
             if player1.sprite.y <= 100:
                 player1.sprite.y = player1.sprite.y
             else:
-                player1.sprite.y -= player1.sprite.playerdata.speed
+                player1.sprite.y -= 4 + (player1.sprite.playerdata.speed)
 
         if keystatus[sdl2.SDL_SCANCODE_S]:
             player1.vy = player1.sprite.playerdata.speed
             if player1.sprite.y >= 620:
                 player1.sprite.y = player1.sprite.y
             else:
-                player1.sprite.y += player1.sprite.playerdata.speed
+                player1.sprite.y += 4 + (player1.sprite.playerdata.speed)
 
         if keystatus[sdl2.SDL_SCANCODE_UP]:
             player2.vy = player2.sprite.playerdata.speed
             if player2.sprite.y <= 100:
                 player2.sprite.y = player2.sprite.y
             else:
-                player2.sprite.y -= player2.sprite.playerdata.speed
+                player2.sprite.y -= 4 + (player2.sprite.playerdata.speed)
 
         if keystatus[sdl2.SDL_SCANCODE_DOWN]:
             player2.vy = player2.sprite.playerdata.speed
             if player2.sprite.y >= 620:
                 player2.sprite.y = player2.sprite.y
             else:
-                player2.sprite.y += player2.sprite.playerdata.speed
+                player2.sprite.y += 4 + (player2.sprite.playerdata.speed)
 
+        """
         for i in range(len(player1missiles)-1):
             if player1missiles[i].sprite.x < 900: #size of the screen
                 player1missiles[i].sprite.x += 3
-
+        """
 
         """
         if event.type == sdl2.SDL_KEYDOWN:
